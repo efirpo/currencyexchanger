367cbfedfd1ffb09196f2bd17d6ed74e78f2a2e4
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-04-24 11:33:24 -0700

    begin defining values of MoneyExchanger object with UI input

--- src/main.js
+++ src/main.js
@@ -13,13 +13,18 @@ $(document).ready(function () {
       console.log(moolah);
       console.log(amount);
       console.log(changeTo);
-      const response = await moolah.exchangeCurrency(amount, "USD", changeTo);
+      const response = await moolah.exchangeCurrency();
+      moolah.conversionRates = response.conversion_rates
+      moolah.baseAmount = amount
+      moolah.targetCurrency = changeTo
+      moolah.conversionRate = response.conversion_rates.changeTo.val()
+      console.log(moolah)
       console.log(response);
-      function getAmount(response) {
-        $("#results").append(`${response.conversion_rate} <br> ${response.conversion_result} <br> ${response.target}`)
+      // function getAmount(response) {
+      //   $("#results").append(`${response.conversion_rate} <br> ${response.conversion_result} <br> ${response.target}`)
 
-      }
-      getAmount(response);
+      // }
+      // getAmount(response);
     })();
   });
 
