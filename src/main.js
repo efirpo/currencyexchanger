f9584545957a5c72caedc06e4d2003ab24fd5055
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-04-24 10:49:57 -0700

    add UI logic and submit button

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import 'bootstrap';
 import './styles.css';
 import { MoneyExchanger } from './../src/currency.js'
-import $ from "jquery";
+import $ from 'jquery';
 
 $(document).ready(function () {
-  let moolah = new MoneyExchanger();
-  return moolah;
-}
+  $("#moneyForm").submit(function (event) {
+    event.preventDefault();
+    (async () => {
+      let moolah = new MoneyExchanger();
+      let amount = $("#amount").val();
+      let changeTo = $("#target").val();
+      console.log(moolah);
+      console.log(amount);
+      console.log(changeTo);
+      const response = await moolah.exchangeCurrency(amount, "USD", changeTo);
+      console.log(response);
+      function getAmount(response) {
+        $("#results").append(`${response.conversion_rate} <br> ${response.conversion_result} <br> ${response.target}`)
+
+      }
+      getAmount(response);
+    })
+  })
+
+});
